8f2c4bddeb1ed8ccc09f2697ecdf4b1a39f12fc5
yunekit <j66x@163.com>
2024-10-15 14:57:37 +0800

    update

--- demos_src/OwinTest/Controllers/ValuesController.cs
+++ demos_src/OwinTest/Controllers/ValuesController.cs
@@ -3,34 +3,47 @@ using System.Web.Http;
 
 namespace OwinTest.Controllers
 {
+    /// <summary>
+    /// WebApi控制器：Values
+    /// </summary>
     public class ValuesController : ApiController
     {
+
+        static readonly List<string> _values = new List<string> { "value1", "value2", "value3" };
+
+
+
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return _values;
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            
-            return "value";
+            if (id < 0 || id > _values.Count) return "NULL.";
+            return _values[id];
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        public void Post([FromBody] string value)
         {
+            _values.Add(value);
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody] string value)
         {
+            if (id < 0 || id > _values.Count) return;
+            _values[id] = value;
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            if (id < 0 || id > _values.Count) return;
+            _values.RemoveAt(id);
         }
     }
 }
--- demos_src/OwinTest/Startup.cs
+++ demos_src/OwinTest/Startup.cs
@@ -3,19 +3,33 @@ using System.Web.Http;
 
 namespace OwinTest
 {
+    /// <summary>
+    /// OWIN启动配置类
+    /// </summary>
     public class Startup
     {
+
+        /// <summary>
+        /// OWIN配置
+        /// </summary>
+        /// <param name="appbuilder"></param>
         public void Configuration(IAppBuilder appbuilder)
         {
-
             var config = new HttpConfiguration();
             ApiRouteConfig(config.Routes);
+
+            //启用WebApi支持
             appbuilder.UseWebApi(config);
-            appbuilder.MapSignalR(); //path: /signalr
+
+            //启用SignalR并默认映射到“/signalr”路径上
+            appbuilder.MapSignalR();
         }
 
 
-
+        /// <summary>
+        /// WebApi路由配置
+        /// </summary>
+        /// <param name="routes"></param>
         void ApiRouteConfig(HttpRouteCollection routes)
         {
             //忽略的路径
