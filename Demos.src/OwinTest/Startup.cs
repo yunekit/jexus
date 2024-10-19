0329b1717145482bad39d254ffe076c1612c28bc
yunekit <j66x@163.com>
2024-10-19 11:15:39 +0800

    update class Startup

--- Demos.src/OwinTest/Startup.cs
+++ Demos.src/OwinTest/Startup.cs
@@ -12,29 +12,32 @@ namespace OwinTest
         /// <summary>
         /// OWIN配置
         /// </summary>
-        /// <param name="appbuilder"></param>
-        public void Configuration(IAppBuilder appbuilder)
+        /// <param name="builder"></param>
+        public void Configuration(IAppBuilder builder)
         {
-            var config = new HttpConfiguration();
-            ApiRouteConfig(config.Routes);
+            //实例化http/webapi配置
+            var httpCconf = new HttpConfiguration();
+            //配置webapi路由
+            ApiRouteConfig(httpCconf.Routes);
 
-            //启用WebApi支持
-            appbuilder.UseWebApi(config);
+            //根据配置启用WebApi支持
+            builder.UseWebApi(httpCconf);
 
             //启用SignalR并默认映射到“/signalr”路径上
-            appbuilder.MapSignalR();
+            builder.MapSignalR();
         }
 
 
         /// <summary>
         /// WebApi路由配置
         /// </summary>
-        /// <param name="routes"></param>
+        /// <param name="routes">路由集合</param>
         void ApiRouteConfig(HttpRouteCollection routes)
         {
             //忽略的路径
             routes.IgnoreRoute("ignore1", "{resource}.axd/{*pathInfo}");
-
+            
+            //默认配置
             routes.MapHttpRoute("default.", "api/{controller}/{id}", new { id = RouteParameter.Optional });
 
         }
